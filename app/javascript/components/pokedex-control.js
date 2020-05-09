f2e9fdb5e5569646445c713348b69b22f6119813
Leon Lee <jtllee1@gmail.com>
2020-05-09 14:19:18 +0100

    added reset all function to red button

--- app/javascript/components/pokedex-control.js
+++ app/javascript/components/pokedex-control.js
@@ -1,6 +1,9 @@
 const pokedexControl = () => {
   const up = document.getElementById("up");
   const down = document.getElementById("down");
+  const redButton = document.getElementById("red-button");
+  const all_images_1 = document.querySelectorAll(".image-1");
+  const all_images_2 = document.querySelectorAll(".image-2");
 
   up.addEventListener('click', () => {
     const active = document.querySelector(".active");
@@ -16,6 +19,11 @@ const pokedexControl = () => {
     const image_2 = active.querySelector(".image-2");
     image_1.classList.toggle('invisible');
     image_2.classList.toggle('invisible');
+  });
+
+  redButton.addEventListener('click', () => {
+    all_images_1.forEach(image => image.classList.remove("invisible"));
+    all_images_2.forEach(image => image.classList.add("invisible"));
   });
 }
 
