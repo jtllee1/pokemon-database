2a9fa73904ceb83c2e41ed568189f22666590570
Leon Lee <jtllee1@gmail.com>
2020-05-10 10:20:48 +0100

    moved pokedex to home and added open close feature

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,9 @@ import "bootstrap";
 // import { fetchKantoPokemon } from '../components/fetch-kanto-pokemon';
 import { pokedexControl } from '../components/pokedex-control';
 import { search } from '../components/search';
+import { pokedexOpenClose } from '../components/open-close';
 
 // fetchKantoPokemon();
 pokedexControl();
 search();
+pokedexOpenClose();
