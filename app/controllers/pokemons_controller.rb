b02c7f41bc41b9a1c52af5649fb9ec91b539ae07
Leon Lee <jtllee1@gmail.com>
2020-05-11 14:58:17 +0100

    removed index from pokemon

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,9 +1,5 @@
 class PokemonsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
-
-  def index
-    @pokemons = Pokemon.all
-  end
+  skip_before_action :authenticate_user!, only: [:show]
 
   def show
     @pokemon = Pokemon.find(params[:id])
