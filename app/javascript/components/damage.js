2ffc78bb9fe907475fa29c0db96e453b4273433a
Leon Lee <jtllee1@gmail.com>
2020-05-22 22:14:56 +0100

    added damage calculations to health bar

--- app/javascript/components/damage.js
+++ app/javascript/components/damage.js
@@ -1,5 +1,12 @@
-const damage = () => {
-
+const damages = (pokemon1, pokemon2, move) => {
+  if (move.damageClass === "Physical") {
+    var offence = pokemon1.atk
+    var defence = pokemon2.def
+  } else if (move.damageClass === "Special") {
+    var offence = pokemon1.spatk
+    var defence = pokemon2.spdef
+  };
+  return ((42 * move.power * (offence/defence))/50) + 2;
 }
 
-export { damage };
+export { damages };
--- app/javascript/components/pokemon-create.js
+++ app/javascript/components/pokemon-create.js
@@ -1,5 +1,21 @@
-const pokemonCreate = () => {
+const pokemonCreate = (pokemon1, pokemon2) => {
+  pokemon1["hp"] = document.getElementById("hp-1").innerText;
+  pokemon1["atk"] = document.getElementById("atk-1").innerText;
+  pokemon1["def"] = document.getElementById("def-1").innerText;
+  pokemon1["spatk"] = document.getElementById("spatk-1").innerText;
+  pokemon1["spdef"] = document.getElementById("spdef-1").innerText;
+  pokemon1["spd"] = document.getElementById("spd-1").innerText;
+  pokemon1["type-1"] = document.getElementById("1-type-1").innerText;
+  pokemon1["type-2"] = document.getElementById("1-type-2").innerText;
 
+  pokemon2["hp"] = document.getElementById("hp-2").innerText;
+  pokemon2["atk"] = document.getElementById("atk-2").innerText;
+  pokemon2["def"] = document.getElementById("def-2").innerText;
+  pokemon2["spatk"] = document.getElementById("spatk-2").innerText;
+  pokemon2["spdef"] = document.getElementById("spdef-2").innerText;
+  pokemon2["spd"] = document.getElementById("spd-2").innerText;
+  pokemon1["type-1"] = document.getElementById("2-type-1").innerText;
+  pokemon1["type-2"] = document.getElementById("2-type-2").innerText;
 }
 
 export { pokemonCreate };
