3cf695f5b8d7a5c3f8bcd84adf1c600020467b4b
Leon Lee <jtllee1@gmail.com>
2020-05-25 20:05:21 +0100

    added control pad options

--- app/javascript/components/move.js
+++ app/javascript/components/move.js
@@ -9,6 +9,10 @@ const move = () => {
   const moveTwo = document.getElementById("move-2");
   const moveThree = document.getElementById("move-3");
   const moveFour = document.getElementById("move-4");
+  const up = document.getElementById("up-control");
+  const right = document.getElementById("right-control");
+  const down = document.getElementById("down-control");
+  const left = document.getElementById("left-control");
 
   moveOne.addEventListener('click', () => {
     moveOne.classList.add("active");
@@ -91,6 +95,114 @@ const move = () => {
         moveFour.classList.remove("active");
       }
     };
+  });
+
+  up.addEventListener('click', () => {
+    if (moveOne.classList.contains("active")) {
+      moveOne.classList.remove("active");
+      moveTwo.classList.remove("active");
+      moveThree.classList.add("active");
+      moveFour.classList.remove("active");
+    }
+    else if (moveTwo.classList.contains("active")) {
+      moveOne.classList.remove("active");
+      moveTwo.classList.remove("active");
+      moveThree.classList.remove("active");
+      moveFour.classList.add("active");
+    }
+    else if (moveThree.classList.contains("active")) {
+      moveOne.classList.add("active");
+      moveTwo.classList.remove("active");
+      moveThree.classList.remove("active");
+      moveFour.classList.remove("active");
+    }
+    else if (moveFour.classList.contains("active")) {
+      moveOne.classList.remove("active");
+      moveTwo.classList.add("active");
+      moveThree.classList.remove("active");
+      moveFour.classList.remove("active");
+    }
+  });
+
+  down.addEventListener('click', () => {
+    if (moveOne.classList.contains("active")) {
+      moveOne.classList.remove("active");
+      moveTwo.classList.remove("active");
+      moveThree.classList.add("active");
+      moveFour.classList.remove("active");
+    }
+    else if (moveTwo.classList.contains("active")) {
+      moveOne.classList.remove("active");
+      moveTwo.classList.remove("active");
+      moveThree.classList.remove("active");
+      moveFour.classList.add("active");
+    }
+    else if (moveThree.classList.contains("active")) {
+      moveOne.classList.add("active");
+      moveTwo.classList.remove("active");
+      moveThree.classList.remove("active");
+      moveFour.classList.remove("active");
+    }
+    else if (moveFour.classList.contains("active")) {
+      moveOne.classList.remove("active");
+      moveTwo.classList.add("active");
+      moveThree.classList.remove("active");
+      moveFour.classList.remove("active");
+    }
+  });
+
+  right.addEventListener('click', () => {
+    if (moveOne.classList.contains("active")) {
+      moveOne.classList.remove("active");
+      moveTwo.classList.add("active");
+      moveThree.classList.remove("active");
+      moveFour.classList.remove("active");
+    }
+    else if (moveTwo.classList.contains("active")) {
+      moveOne.classList.add("active");
+      moveTwo.classList.remove("active");
+      moveThree.classList.remove("active");
+      moveFour.classList.remove("active");
+    }
+    else if (moveThree.classList.contains("active")) {
+      moveOne.classList.remove("active");
+      moveTwo.classList.remove("active");
+      moveThree.classList.remove("active");
+      moveFour.classList.add("active");
+    }
+    else if (moveFour.classList.contains("active")) {
+      moveOne.classList.remove("active");
+      moveTwo.classList.remove("active");
+      moveThree.classList.add("active");
+      moveFour.classList.remove("active");
+    }
+  });
+
+  left.addEventListener('click', () => {
+    if (moveOne.classList.contains("active")) {
+      moveOne.classList.remove("active");
+      moveTwo.classList.add("active");
+      moveThree.classList.remove("active");
+      moveFour.classList.remove("active");
+    }
+    else if (moveTwo.classList.contains("active")) {
+      moveOne.classList.add("active");
+      moveTwo.classList.remove("active");
+      moveThree.classList.remove("active");
+      moveFour.classList.remove("active");
+    }
+    else if (moveThree.classList.contains("active")) {
+      moveOne.classList.remove("active");
+      moveTwo.classList.remove("active");
+      moveThree.classList.remove("active");
+      moveFour.classList.add("active");
+    }
+    else if (moveFour.classList.contains("active")) {
+      moveOne.classList.remove("active");
+      moveTwo.classList.remove("active");
+      moveThree.classList.add("active");
+      moveFour.classList.remove("active");
+    }
   });
 }
 
