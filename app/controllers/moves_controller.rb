ee30fd4396e63db54f51c4d0514bf5ed1160bc1b
Leon Lee <jtllee1@gmail.com>
2020-06-11 19:07:44 +0100

    removed show and moves pages

--- app/controllers/moves_controller.rb
+++
@@ -1,7 +0,0 @@
-class MovesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
-
-  def index
-    @moves = Move.all
-  end
-end
--- app/controllers/pokemons_controller.rb
+++
@@ -1,8 +0,0 @@
-class PokemonsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:show]
-
-  def show
-    @pokemon = Pokemon.find(params[:id])
-    @moves = PokemonMove.where(pokemon_id: params[:id])
-  end
-end
